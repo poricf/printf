877bdcf2ebba67807d3887d8adee4dbf1d9a1b8f
Samuel Keno <sgere195@gmail.com>
2023-08-19 15:43:21 -0700

    task -0

--- first.c
+++ first.c
@@ -2,6 +2,7 @@
 #include <stdarg.h>
 #include <stdlib.h>
 
+void print_string(char *s);
 
 /**
  * _printf - prints out the parameters given to it
@@ -13,19 +14,16 @@
 int _printf(const char *format, ...)
 {
 	va_list arr;
-	int i, j, count;
-	char *s;
-	char a;
-	
-	count = 0;
+	int i = 0, count = 0;
+	char *s, a;
+
 	va_start(arr, format);
 	if (format != NULL)
 	{
-		i = 0;
 		while (format[i] != '\0')
 		{
 			count++;
-			if (format[i] =='%' && format[i + 1] == '%')
+			if (format[i] == '%' && format[i + 1] == '%')
 			{
 				count--;
 				_putchar('%');
@@ -42,10 +40,12 @@ int _printf(const char *format, ...)
 						break;
 					case 's':
 						s = va_arg(arr, char *);
-						for (j = 0; s[j] != '\0'; j++)
-							_putchar(s[j]);
+						print_string(s);
 						break;
 					case 'd':
+						print_num(va_arg(arr, int));
+						break;
+					case 'i':
 						print_num(va_arg(arr, int));
 						break;
 					default:
@@ -54,17 +54,19 @@ int _printf(const char *format, ...)
 						continue;
 				}
 			}
-			else
-			{
+			else if (format[i] != '%')
 				_putchar(format[i]);
-		
-			}
 			i++;
 		}
 	}
-
 	va_end(arr);
-
-
 	return (count);
+}
+
+void print_string(char *s)
+{
+	int k;
+
+	for (k = 0; s[k]; k++)
+		_putchar(s[k]);
 }
--- main.h
+++ main.h
@@ -4,5 +4,6 @@
 #include <stdio.h>
 
 int _putchar(char c);
-
+int _printf(const char *format, ...);
+void print_num(int num);
 #endif /* MAIN_H */
